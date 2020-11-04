5e005ba9d28df680aaae93adc20efa4bb34deab7
Taylor Phelps <tphelps1993@gmail.com>
2020-11-04 14:39:31 -0500

    heroku push

--- src/app.js
+++ src/app.js
@@ -4,18 +4,17 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const articlesRouter = require('./articles/articles-router');
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.use('/api/articles', articlesRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
